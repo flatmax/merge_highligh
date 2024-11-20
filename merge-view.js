a5b4bcfc41db88ef208f540be24eb7b18d86ceeb
Matt Flax <flatmax@flatmax.org>
2024-11-20 11:54:51 +1100

    Add language autodetection and syntax highlighting
    - Add support for multiple programming languages
    - Implement automatic language detection based on code content
    - Add language mode scripts (Python, XML, CSS, etc.)
    - Update test code to demonstrate Python syntax highlighting

--- merge-view.js
+++ merge-view.js
@@ -66,6 +66,31 @@ export class MergeView extends LitElement {
         this._resizeObserver = null;
     }
 
+    detectLanguage(code) {
+        if (!code) return 'javascript'; // default
+        
+        // Common patterns for language detection
+        const patterns = {
+            python: /^(def|class|import|from|if __name__|print)\b|\.py$/i,
+            javascript: /^(function|const|let|var|import|class)\b|\.js$/i,
+            html: /^<!DOCTYPE|<html|<head|<body|\.html$/i,
+            css: /^(\.|#|@media|@import)|{|}|\.css$/i,
+            java: /^(public|private|protected|class|interface|enum)\b|\.java$/i,
+            cpp: /^(#include|using namespace|int main|class.*{)|\.cpp$/i,
+            ruby: /^(def|class|require|module|gem)\b|\.rb$/i,
+            php: /^(<\?php|\$[a-zA-Z_]|function|class|namespace)\b|\.php$/i
+        };
+
+        // Check each pattern
+        for (const [lang, pattern] of Object.entries(patterns)) {
+            if (pattern.test(code)) {
+                return lang;
+            }
+        }
+
+        return 'javascript'; // fallback
+    }
+
     disconnectedCallback() {
         super.disconnectedCallback();
         if (this._resizeObserver) {
@@ -132,11 +157,21 @@ export class MergeView extends LitElement {
             const cursor = mv.edit.getCursor();
             mv.edit.setValue(this.leftText);
             mv.edit.setCursor(cursor);
+            // Update mode based on new content
+            const detectedMode = this.detectLanguage(this.leftText);
+            mv.edit.setOption('mode', detectedMode);
+            mv.right.edit.setOption('mode', detectedMode);
         }
         if (changedProperties.has('rightText') && !this._ignoreRightChange) {
             const cursor = mv.right.edit.getCursor();
             mv.right.edit.setValue(this.rightText);
             mv.right.edit.setCursor(cursor);
+            // Update mode if right side changed and left is empty
+            if (!this.leftText) {
+                const detectedMode = this.detectLanguage(this.rightText);
+                mv.edit.setOption('mode', detectedMode);
+                mv.right.edit.setOption('mode', detectedMode);
+            }
         }
     }
 
@@ -148,11 +183,14 @@ export class MergeView extends LitElement {
         }
 
         try {
+            // Detect initial language mode
+            const detectedMode = this.detectLanguage(this.leftText || this.rightText);
+            
             this.mergeView = CodeMirror.MergeView(mergeViewElement, {
                 value: this.leftText,
                 orig: this.rightText,
                 lineNumbers: true,
-                mode: 'javascript',
+                mode: detectedMode,
                 highlightDifferences: true,
                 connect: true,
                 collapseIdentical: false,
@@ -174,6 +212,10 @@ export class MergeView extends LitElement {
                 if (this._ignoreLeftChange) return;
                 this._ignoreLeftChange = true;
                 this.leftText = this.mergeView.edit.getValue();
+                // Update mode based on content
+                const detectedMode = this.detectLanguage(this.leftText);
+                this.mergeView.edit.setOption('mode', detectedMode);
+                this.mergeView.right.edit.setOption('mode', detectedMode);
                 this.dispatchEvent(new CustomEvent('left-changed', { detail: this.leftText }));
                 this._ignoreLeftChange = false;
             });
@@ -182,6 +224,12 @@ export class MergeView extends LitElement {
                 if (this._ignoreRightChange) return;
                 this._ignoreRightChange = true;
                 this.rightText = this.mergeView.right.edit.getValue();
+                // Update mode if left side is empty
+                if (!this.leftText) {
+                    const detectedMode = this.detectLanguage(this.rightText);
+                    this.mergeView.edit.setOption('mode', detectedMode);
+                    this.mergeView.right.edit.setOption('mode', detectedMode);
+                }
                 this.dispatchEvent(new CustomEvent('right-changed', { detail: this.rightText }));
                 this._ignoreRightChange = false;
             });
